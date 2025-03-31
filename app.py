a54919774b91ae4e55c334f55020bd37bd73c6d4
Maria <maria.ylonen2@gmail.com>
2025-03-31 06:06:39 +0300

    Add new items

--- app.py
+++ app.py
@@ -10,10 +10,27 @@ app.secret_key = config.secret_key
 @app.route("/")
 def index():
     return render_template("index.html")
+@app.route("/new_item")
+def new_item():
+    return render_template("new_item.html")
+
+@app.route("/create_item", methods=["POST"])
+def create_item():
+    title = request.form["title"]
+    description = request.form["description"]
+    city = request.form["city"]
+    user_id = session["user_id"]
+
+    sql = """INSERT INTO items(title,description,city,user_id) 
+            VALUES (?,?,?,?)"""
+    db.execute(sql, [title, description, city, user_id])
+
+    return redirect("/")
 
 @app.route("/register")
 def register():
     return render_template("register.html")
+
 
 @app.route("/create", methods=["POST"])
 def create():
@@ -31,24 +48,30 @@ def create():
         return "VIRHE: tunnus on jo varattu"
 
     return "Tunnus luotu"
+
 @app.route("/login", methods=["GET","POST"])
 def login():
     if request.method == "GET":
         return render_template("login.html")
+    
     if request.method == "POST":
         username = request.form["username"]
         password = request.form["password"]
     
-        sql = "SELECT password_hash FROM users WHERE username = ?"
-        password_hash = db.query(sql, [username])[0][0]
+        sql = "SELECT id, password_hash FROM users WHERE username = ?"
+        result = db.query(sql, [username])[0]
+        user_id = result["id"]
+        password_hash = result["password_hash"]
 
         if check_password_hash(password_hash, password):
+            session["user_id"] = user_id
             session["username"] = username
             return redirect("/")
         else:
             return "VIRHE: väärä tunnus tai salasana"
 
-@app.route("/logout")
+@app.route("/logout", methods=["GET"])
 def logout():
+    del session["user_id"]
     del session["username"]
     return redirect("/")
