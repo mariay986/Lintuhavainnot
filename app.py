38bb053bcd6a243f9de607899f924b0ac0546b6e
Maria <maria.ylonen2@gmail.com>
2025-04-03 21:12:00 +0300

    Add not found handling

--- app.py
+++ app.py
@@ -27,6 +27,8 @@ def find_item():
 @app.route("/item/<int:item_id>")
 def show_item(item_id):
     item = items.get_item(item_id)
+    if not item:
+        abort(404)
     return render_template("show_item.html", item=item)
     
 @app.route("/new_item")
@@ -45,6 +47,8 @@ def create_item():
 @app.route("/edit_item/<int:item_id>")
 def edit_item(item_id):
     item = items.get_item(item_id)
+    if not item:
+        abort(404)
     if item["user_id"] != session["user_id"]:
         abort(403)
     return render_template("edit_item.html", item=item)
@@ -53,6 +57,8 @@ def edit_item(item_id):
 def update_item():
     item_id = request.form["item_id"]
     item = items.get_item(item_id)
+    if not item:
+        abort(404)
     if item["user_id"] != session["user_id"]:
         abort(403)
     title = request.form["title"]
@@ -65,6 +71,8 @@ def update_item():
 @app.route("/remove_item/<int:item_id>", methods=["GET","POST"])
 def remove_item(item_id):
     item = items.get_item(item_id)
+    if not item:
+        abort(404)
     if item["user_id"] != session["user_id"]:
         abort(403)
 
